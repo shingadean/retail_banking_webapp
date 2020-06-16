862a8fbb4abde261e3c926da6a94a69abe1779a8
Abhijit05051998 <narayanan.abhi16@gmail.com>
2020-06-16 11:17:06 +0530

    delete account close account login page

--- application/routes.py
+++ application/routes.py
@@ -3,10 +3,23 @@ from flask import render_template, request, json, Response, redirect, flash, url
 from flask_restplus import Resource
 from application.data_country import states_list
 
-@app.route('/login')
-def login():
-    return render_template("login.html", login=True)
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 @app.route('/create_customer')
 def create_customer():
     return render_template("create_customer.html", title="Create Customer", states=states_list)
+
+@app.route('/close_account')
+def close_account():
+    return render_template("close account.html")
+
+
+@app.route('/delete_account')
+def delete_account():
+    return render_template("delete_account.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
