a522a4cd6a7a3190e4b6f1e2dc8915f36bb48e22
shingadean <amitshingade1march98@gmail.com>
2020-06-16 20:45:19 +0530

    update 2

--- application/routes.py
+++ application/routes.py
@@ -12,9 +12,31 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template("login.html", login=True)
+    return render_template("login.html", login=True, title="LOGIN")
 
 @app.route('/create_customer')
 def create_customer():
     return render_template("create_customer.html", title="Create Customer", states=states_list)
+
+@app.route('/update_customer')
+def update_customer():
+    return render_template("update_customer.html", title="Update Customer")
+
+
+@app.route('/delete_account')
+def delete_account():
+    return render_template("delete_account.html", title="Delete Account")
+
+@app.route('/create_account')
+def create_account():
+    return render_template("create_account.html", title="Create Account")
+
+@app.route('/deposit_amount')
+def deposit_amount():
+    return render_template("deposit_amount.html", title="Cash Deposit")
+
+
+@app.route('/withdraw_amount')
+def withdraw_amount():
+    return render_template("withdraw_amount.html", title="Withdraw Cash")
 
